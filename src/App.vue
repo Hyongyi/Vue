afb1e2065888ff73bcb626d1dcaa99b318933c77
Hyoyoung <gyddl123@gmail.com>
2022-11-10 11:26:05 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{logo}}
+    <h4>XX 원룸</h4>   <!-- 데이터를 집어넣고 싶을시 {{변수명}} -->
+    <p>{{price1}}만원</p>
+  </div>
+    <div>
+    <h4>XX 원룸</h4>
+    <p>{{price2}} 만원</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      //데이터 보관 장소
+      price1 : 60,
+      price2 : 70,
+      logo : '원룸샵'
+
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
