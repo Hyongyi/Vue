5b788ff48026967941bdd0582ac96fccdf9da449
Hyoyoung <gyddl123@gmail.com>
2022-12-14 05:25:35 +0900

    vuedongsan complete

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <div>
-    {{logo}}
-    <h4>XX 원룸</h4>   <!-- 데이터를 집어넣고 싶을시 {{변수명}} -->
-    <p>{{price1}}만원</p>
+  <div class="menu">
+    <a v-for="(a,i) in menu" :key="i">{{ a }}</a>
   </div>
-    <div>
-    <h4>XX 원룸</h4>
-    <p>{{price2}} 만원</p>
+
+  <div class="black-bg" v-if="modal == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>내용</p>
+      <button @click="modal = false">닫기</button>
+    </div>
+  </div>
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+
+<div v-for="(a,i) in 6" :key="i">
+    <img :src='oneroom[i].image' alt="사진1" class="room-img"> 
+    <h4>{{oneroom[i].title}}</h4> 
+    <p>{{oneroom[i].price}}원</p>
+    <p>{{oneroom[i].content}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import data from './assets/oneroom.js'
 
 export default {
   name: 'App',
   data() {
     return {
       //데이터 보관 장소
-      price1 : 60,
-      price2 : 70,
-      logo : '원룸샵'
+      oneroom :data,
+      modal : false,
+      img : ['room0','room1','room2'],
+      price : [50,60,70],
+      menu : ['Home', 'Shop', 'About'],
+      products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+      
 
     }
   },
+  methods : {
+ 
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
@@ -39,6 +58,43 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+body {
+  margin: 0;
+}
+
+div{
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img {width: 100%;
+margin-top: 40px;}
+
+.menu {
+  background: darkslateblue;
+  padding :15px;
+  border-radius:  5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
